411d119881a6ddde166b9d6b1e78f55b5b66e318
xsebassj <tian10122017@gmail.com>
2025-08-08 17:19:16 -0300

    agregando migraciones

--- blognoticias/apps/usuarios/models.py
+++ blognoticias/apps/usuarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class usuario(models.Model):
+
+    nombre= models.CharField(max_length=50, blank= False , null= False)
+    apellido = models.CharField(max_length=50, blank= False , null= False)
+    
+
+    
--- blognoticias/blognoticias/configurations/base.py
+++ blognoticias/blognoticias/configurations/base.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.noticias',
+    'apps.usuarios',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'blognoticias.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
