c6908ee96946a78b1330920a28f75c924853d8d9
xsebassj <tian10122017@gmail.com>
2025-08-08 17:41:42 -0300

    agregando models a noticias y usuarios

--- blognoticias/apps/noticias/models.py
+++ blognoticias/apps/noticias/models.py
@@ -1,26 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import User
+from usuarios.models import Persona
 from django.utils import timezone
 
 # Create your models here.
 
 
 class Noticia(models.Model):
-    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Persona, on_delete=models.CASCADE)
     titulo = models.CharField(max_length= 100, blank=False , null=False)
     contenido = models.TextField()
     fecha_publicacion = models.DateTimeField(auto_now_add= True)
     imagen = models.ImageField ( upload_to='',blank=True,null= True)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.titulo
 
 class Comentario(models.Model):
     noticia = models.ForeignKey(Noticia, on_delete=models.CASCADE, related_name= 'comentarios')
-    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Persona, on_delete=models.CASCADE)
     contenido = models.TextField()
     fecha_publicado = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
-        return f'comentario de {self.autor.username} en "{self.noticia.titulo}"'
+    def __str__(self):
+        return f'comentario de {self.autor} en "{self.noticia.titulo}"'
     
