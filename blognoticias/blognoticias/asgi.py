b40f03b8ddd884fb9726f5298b4970378cd06a87
xsebassj <tian10122017@gmail.com>
2025-08-08 16:21:55 -0300

    agregando apps

--- blognoticias/blognoticias/asgi.py
+++ blognoticias/blognoticias/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blognoticias.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blognoticias.configurations')
 
 application = get_asgi_application()
