d5da48c43eba62ce3f1b1bf1d4261deac00da210
xsebassj <tian10122017@gmail.com>
2025-08-09 12:50:48 -0300

    agregando models a noticias

--- blognoticias/apps/noticias/models.py
+++ blognoticias/apps/noticias/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+import uuid, os
+from django.conf import settings
+from django.utils import timezone
 
 
+class post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    titulo = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, max_length=100 , blank=True, null=True)
+    contenido = models.TextField()
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    update_at = models.DateTimeField(auto_now=True)
+
+    
 
