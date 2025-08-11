29e320fca375e25ef9b1ceda583a0a3933532a75
xsebassj <tian10122017@gmail.com>
2025-08-11 07:26:54 -0300

    Update views.py

--- blognoticias/blognoticias/views.py
+++ blognoticias/blognoticias/views.py
@@ -5,7 +5,3 @@ def inicio_view(request):
     posts = Post.objects.all().order_by("-created_at")[:6]
     return render(request, "inicio.html", {"posts": posts})
 
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("¡Hola Sebas! Django está vivo y respondiendo.")
