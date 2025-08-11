0211002895c82c5a4302e7969c691d8b7041bff2
xsebasssj <tian10122017@gmail.com>
2025-08-11 10:53:17 +0000

    Corrijo configuración WSGI para producción

--- blognoticias/blognoticias/wsgi.py
+++ blognoticias/blognoticias/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blognoticias.configurations.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blognoticias.configurations.production')
 
 application = get_wsgi_application()
