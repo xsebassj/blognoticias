1cd3df238756f882a9f074fe0f689bedf55fc2b7
xsebassj <tian10122017@gmail.com>
2025-08-11 05:19:05 -0300

    Update base.py

--- blognoticias/blognoticias/configurations/base.py
+++ blognoticias/blognoticias/configurations/base.py
@@ -21,14 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'default'
+SECRET_KEY = "default"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-
-
-
-SECRET_KEY = ´default'
 
 
 
