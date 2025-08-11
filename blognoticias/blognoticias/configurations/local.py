5493d9bf8f921495831ffc357a9ab3724775b062
xsebassj <tian10122017@gmail.com>
2025-08-11 05:14:15 -0300

    Update local.py

--- blognoticias/blognoticias/configurations/local.py
+++ blognoticias/blognoticias/configurations/local.py
@@ -3,7 +3,7 @@ from decouple import config,Csv
 DEBUG = True
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-DATABASES = os.getenv('DJANGO_DATABASE')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
