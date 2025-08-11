56522dca9987f6e12f1892e5d7375335be7c575c
xsebassj <tian10122017@gmail.com>
2025-08-11 04:55:56 -0300

    cambiando db

--- blognoticias/blognoticias/configurations/base.py
+++ blognoticias/blognoticias/configurations/base.py
@@ -26,7 +26,18 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = []
+from decouple import config,Csv
+
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+DEBUG = config('DJANGO_DEBUG', cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Application definition
--- blognoticias/blognoticias/configurations/local.py
+++ blognoticias/blognoticias/configurations/local.py
@@ -1,7 +1,12 @@
 from .base import * 
-
-DEBUG = True
-
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS')
+from decouple import config,Csv
+DEBUG = config('DJANGO_DEBUG', cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 DATABASES = os.getenv('DJANGO_DATABASE')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
